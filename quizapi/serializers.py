bed1eb7e0084e12dd9ee6951938ae031acf18543
Titov Artem <pbi62007@yandex.ru>
2016-09-20 19:48:40 +0300

    issue #3 Add ability to register a new users.

--- quizapi/serializers.py
+++ quizapi/serializers.py
@@ -6,7 +6,29 @@ from . models import Test, Task, Subject, PossibleAnswer, UserAnswer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'groups')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name',
+                  'groups', 'password')
+        write_only_fields = ('password',)
+        read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super(UserSerializer, self).update(instance, validated_data)
 
 
 class GroupSerializer(serializers.ModelSerializer):
